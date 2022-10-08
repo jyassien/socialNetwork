ba3329e8cbb5accc8eb29cbd7f2bd0d5713fa035
Jihadu Yassien <65809610+jyassien@users.noreply.github.com>
2022-10-07 23:31:06 -0600

    Simple access to index.ejs using main route configured.

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const session = require("express-session");
 const MongoStore = require("connect-mongo")(session);
 const methodOverride = require("method-override");
 const flash = require("express-flash");
-const logger = require("morgan");
+// const logger = require("morgan");
 // const connectDB = require("./config/database");
 const mainRoutes = require("./routes/main");
 // const postRoutes = require("./routes/posts");
@@ -23,37 +23,40 @@ const app = express();
 app.set("view engine", "ejs");
 
 //Static Folder
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 //Body Parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //Logging
-app.use(logger("dev"));
+// app.use(logger("dev"));
 
 //Use forms for put / delete
-app.use(methodOverride("_method"));
+// app.use(methodOverride("_method"));
 
 // Setup Sessions - stored in MongoDB. Login status will be kept even if the browser is closed.
-app.use(
-  session({
-    secret: "keyboard cat",
-    resave: false,
-    saveUninitialized: false,
-    store: new MongoStore({ mongooseConnection: mongoose.connection }),
-  })
-);
+// app.use(
+//   session({
+//     secret: "keyboard cat",
+//     resave: false,
+//     saveUninitialized: false,
+//     store: new MongoStore({ mongooseConnection: mongoose.connection }),
+//   })
+// );
 
 // Passport middleware
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 //Use flash messages for errors, info, ect...
-app.use(flash());
+// app.use(flash());
 
 //Setup Routes For Which The Server Is Listening
-// app.use("/", mainRoutes);
+app.get("/try", (req, res) => {
+  res.send("Trial successfull>");
+});
+app.use("/", mainRoutes);
 // app.use("/post", postRoutes);
 
 //Server Running
