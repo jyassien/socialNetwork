76cd0be5a23976d9546efbadd971b8c5599da574
Jihadu Yassien <65809610+jyassien@users.noreply.github.com>
2022-10-07 22:33:54 -0600

    mongodb version downgraded to v3.

--- server.js
+++ server.js
@@ -7,18 +7,17 @@ const MongoStore = require("connect-mongo")(session);
 const methodOverride = require("method-override");
 const flash = require("express-flash");
 const logger = require("morgan");
-const connectDB = require("./config/database");
-const mainRoutes = require("./routes/main");
-const postRoutes = require("./routes/posts");
-
+// const connectDB = require("./config/database");
+// const mainRoutes = require("./routes/main");
+// const postRoutes = require("./routes/posts");
 
 const app = express();
 
 // Passport config
-require("./config/passport")(passport);
+// require("./config/passport")(passport);
 
 //Connect To Database
-connectDB();
+// connectDB();
 
 //Using EJS for views
 app.set("view engine", "ejs");
@@ -54,8 +53,8 @@ app.use(passport.session());
 app.use(flash());
 
 //Setup Routes For Which The Server Is Listening
-app.use("/", mainRoutes);
-app.use("/post", postRoutes);
+// app.use("/", mainRoutes);
+// app.use("/post", postRoutes);
 
 //Server Running
 app.listen(process.env.PORT || 4041, () => {
