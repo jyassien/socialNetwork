4f9508283eb3db4e82047f90d04b8032aa1ddf88
Jihadu Yassien <65809610+jyassien@users.noreply.github.com>
2022-10-09 19:51:11 -0600

    Post.js(A Schema for users post) configured.

---
+++ models/Post.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    require: true,
+  },
+  cloudinaryId: {
+    type: String,
+    required: true,
+  },
+  caption: {
+    type: String,
+    required: true,
+  },
+  likes: {
+    type: Number,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectID,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    Default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Post", PostSchema);
